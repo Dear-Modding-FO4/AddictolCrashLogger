702a80ebe122a0f26333cb395251bf8a9dfd9be1
Kuz <kuzeygokk@outlook.com>
2026-09-09 18:04:40 -0700

    Use CommonLibF4's shared DearModdingUI API

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,5 @@
 -- include subprojects
 includes("lib/commonlibf4")
-includes("lib/dearmoddingui-api")
 
 -- name and version
 local plugin_name = "AddictolCrashLogger"
@@ -60,7 +59,6 @@ target(plugin_name)
     add_packages("rapidcsv")
     add_packages("rsm-binary-io")
     add_packages("zydis")
-    add_deps("dearmoddingui-api")
 
     -- add DIA SDK Includes
     add_includedirs(os.getenv("VSINSTALLDIR") .. "/DIA SDK/include")
